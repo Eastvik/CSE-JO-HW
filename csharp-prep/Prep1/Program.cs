a427dc5b7d6c08232ae1a7bdc9e8d67b54a5bc34
jim ostvig <jhostvig@gmail.com>
2024-09-21 15:24:37 -0600

    Changed the program to instead of printing a message to terminal, it asks prompts to recreate James Bond quotes.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,15 @@
 using System;
-
+//Removed Hello World
+//Why does James Bond use his real name? 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your given name? >");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your surname? >");
+        string last = Console.ReadLine();
+        Console.WriteLine($"Your nmae is {last}, {first} {last}.");
     }
 }
