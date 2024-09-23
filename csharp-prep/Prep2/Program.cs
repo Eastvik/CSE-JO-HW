194bfa34151a56743831ae6a54918f1899e2c0b3
jim ostvig <jhostvig@gmail.com>
2024-09-23 13:53:20 -0600

    Please commit this time

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? >%");
+        //I found out that ctrl f2 also changes printed instances not just variable names.
+        string gradeValue = Console.ReadLine();
+        int gPercent = int.Parse(gradeValue);
+        
+        //define grade letter to replace later
+        string letter ="";
+
+        if (gPercent >= 90)
+        {
+            letter = "A";
+        }
+        else if (gPercent >= 80)
+        {
+            letter = "B";
+        }
+        else if (gPercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gPercent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        
+        //Print statement, this said "letter letter" after changing all occurances of the variable 'grade' to 'letter'
+        Console.WriteLine($"Your grade letter is {letter}");
+
+        if (gPercent >= 70)
+        {
+            Console.WriteLine("Great job, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Please contact your academic advisor, resources will be provided to aid you in the future");
+        }
+        
+
     }
 }
