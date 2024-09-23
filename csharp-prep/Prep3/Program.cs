ad6fc7ac48c9ffab1b99f43b9d8c522849c0f299
jim ostvig <jhostvig@gmail.com>
2024-09-23 14:22:47 -0600

    Changed code to include a random number guessing game, code chooses a number from 0-100, user must guess the numer receiving hints. Comparing my code to the answer code having an else if to see if the number is == does seem a bit silly but I wanted to see if the syntax worked in C# and Python.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomnumber = new Random();
+        int magicnumber = randomnumber.Next(1,101);
+
+        int guess = -1;
+
+        while (guess != magicnumber)
+        { Console.Write("What is your guess? (1-100) >");
+        guess = int.Parse(Console.ReadLine());
+
+        if (magicnumber > guess)
+        {
+            Console.WriteLine("Higher");
+        }
+
+        else if (magicnumber == guess)
+        {
+            Console.WriteLine("CORRECT!!");
+        }
+
+        else
+        {
+            Console.WriteLine("Lower");
+        }
+        }
     }
 }
