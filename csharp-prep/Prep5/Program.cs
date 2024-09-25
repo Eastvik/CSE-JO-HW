e5e79ad36ce48e4b2a68c3da77b7fc58fdbc2e0b
jim ostvig <jhostvig@gmail.com>
2024-09-25 16:58:54 -0600

    Wrote program to use functions to get user info, square a prompted int, and display the square and the username

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string user = PromptUserName();
+    
+        int userNumber = PromptUserNumber();
+       int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(user, squaredNumber);
     }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("What is your username?:>");
+        string nameUser = Console.ReadLine();
+
+        return nameUser;
+    }
+//Kept accidentally putting void on int and string functions, still getting used to C#
+    static int PromptUserNumber()
+    {
+        Console.Write("Please submit a number.:> ");
+        int numbUser = int.Parse(Console.ReadLine());
+
+        return numbUser;
+    }
+    static int SquareNumber(int userNumber)
+    {   //I miss the ** operator for exponents
+        int square = userNumber * userNumber;
+
+        return square;
+    }
+
+    static  void DisplayResult(string user, int squaredNumber)
+    {
+        Console.Write($"{user} the square of your number is {squaredNumber}.");
+
+    }
+
 }
