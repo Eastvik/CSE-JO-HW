d806bfd78346ba9b34fda2ea4f0d2542fd96ab24
jim ostvig <jhostvig@gmail.com>
2024-10-01 10:03:47 -0600

    Code changed to use a job and resume class to print to terminal, I used a constructor in Job instead of the specified dot commands, if that i incorrect please email me and I will comment it out and use dot commands

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,6 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Job job1= new Job("Microsoft", "Software engineer",2019, 2022);
-        job1.Display();
-    }
-}
+        Job job2= new Job("Apple", "Manager",2022, 2023);
+
+        Resume myResume= new Resume();
+        myResume._name = "Allison Rose";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+    }}
