9f886b4bac1940639645ddda7e8f660046ae1339
jim ostvig <jhostvig@gmail.com>
2024-10-01 10:04:46 -0600

    Resume class, can store jobs into a list as objects and assigns a name to them.

--- prepare/Learning02/resume.cs
+++ prepare/Learning02/resume.cs
@@ -4,6 +4,12 @@ public class Resume
     public List<Job> _jobs = new List<Job>();
     public void Display()
     {
-        
+        Console.WriteLine($"Name: {_name}.");
+        Console.WriteLine("Jobs:");
+    
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
     }
 }
