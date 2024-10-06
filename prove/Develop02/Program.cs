04677f465b534eb2739ca27e6efb451558424077
jim ostvig <jhostvig@gmail.com>
2024-10-06 15:10:14 -0600

    Program uses a journal class, an entry class, and a prompt generator class to help record and save journals.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        PromptGenerator promptGen = new PromptGenerator();
+
+        
+        journal.ReadFromFile();
+
+        bool running = true;
+        while (running)
+        {
+            Console.Write("Please input the number: (1)'new journal entry', (2)'read entries', or (0)'save & exit':> ");
+            string answer = Console.ReadLine().ToLower();
+
+            if (answer == "1")
+            {
+                string prompt = promptGen.GetRandomPrompt();
+                Console.WriteLine(prompt);
+
+                string response = Console.ReadLine();
+                Entry entry = new Entry(prompt, response);
+                journal.AddEntry(entry);
+                journal.SaveToFile();
+            }
+            else if (answer == "2")
+            {
+                
+                journal.ReadFromFile();
+            }
+            else if (answer == "0")
+            {
+                running = false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid option, please try again.");
+            }
+        }
+
+        journal.DisplayEntries();
     }
 }
