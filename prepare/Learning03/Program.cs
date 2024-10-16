1ad8deb667dd48561f8c49537359a27a93ae0e61
jim ostvig <jhostvig@gmail.com>
2024-10-16 15:41:50 -0600

    Program done

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFracString());
+        Console.WriteLine(f1.GetDecValue());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFracString());
+        Console.WriteLine(f2.GetDecValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFracString());
+        Console.WriteLine(f3.GetDecValue());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFracString());
+        Console.WriteLine(f4.GetDecValue());
+
     }
 }
