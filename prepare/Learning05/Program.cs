44fd8d6e59b74368a0a664f34d140c601aa5ba24
jim ostvig <jhostvig@gmail.com>
2024-10-24 20:07:27 -0600

    Program submission

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -2,8 +2,22 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning05 World!");
-    }
+static void Main(string[] args)
+{
+    // Previous code for Assignment and MathAssignment
+    Assignment simpleAssignment = new Assignment("Samuel Bennett", "Multiplication");
+    Console.WriteLine(simpleAssignment.GetSummary());
+
+    MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+    Console.WriteLine(mathAssignment.GetSummary());
+    Console.WriteLine(mathAssignment.GetHomeworkList());
+
+    // Create a WritingAssignment object
+    WritingAssignment writingAssignment = new WritingAssignment("Mary Johnson", "Ancient History", "The Rise of the Roman Empire");
+
+    
+    Console.WriteLine(writingAssignment.GetSummary());
+    Console.WriteLine(writingAssignment.GetWritingInformation());
+}
+
 }
