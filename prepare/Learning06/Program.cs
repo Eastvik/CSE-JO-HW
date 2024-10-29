4d0f5db3ac4e9f838b965b6f86b297266c6f10f3
jim ostvig <jhostvig@gmail.com>
2024-10-29 09:44:08 -0600

    Done

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        // Create individual shape instances and test their methods
+        Square square = new Square("Red", 4);
+        Console.WriteLine($"Square - Color: {square.GetColor()}, Area: {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("Blue", 5, 3);
+        Console.WriteLine($"Rectangle - Color: {rectangle.GetColor()}, Area: {rectangle.GetArea()}");
+
+        Circle circle = new Circle("Green", 2.5);
+        Console.WriteLine($"Circle - Color: {circle.GetColor()}, Area: {circle.GetArea()}");
+
+        // Create a list of shapes
+        List<Shape> shapes = new List<Shape> { square, rectangle, circle };
+
+        // Iterate through the list and display color and area
+        Console.WriteLine("\nShape List:");
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
---
+++ prepare/Learning06/Rectangle.cs
@@ -0,0 +1,18 @@
+public class Rectangle : Shape
+{
+    private double _width;
+    private double _height;
+
+    // Constructor
+    public Rectangle(string color, double width, double height) : base(color)
+    {
+        _width = width;
+        _height = height;
+    }
+
+    // Override GetArea to calculate area of the rectangle
+    public override double GetArea()
+    {
+        return _width * _height;
+    }
+}
